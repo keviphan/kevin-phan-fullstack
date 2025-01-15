bc86594d54c168e23fe8e0762e9a1122891b64aa
Kevin Phan <keviphan@pdx.edu>
2025-01-15 01:20:15 -0800

    Wrote fizzbuzz function

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -9,6 +9,22 @@ Use console.log() to write the proper output to the command line.
 
 **/
 
+const fizzbuzz = function fizzbuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+fizzbuzz();
+
 // 1
 // 2
 // fizz
