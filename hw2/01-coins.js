34624855052eb30a58c51769c52cc8cd54b98742
Kevin Phan <keviphan@pdx.edu>
2025-01-31 14:06:02 -0800

    Implemented 01-coins.js and -2-node-routing.js

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,6 +1,33 @@
 /** Exercise 01 - Coins **/
 
-// Add your function here 
+// Add your function here
+const calculateChange = (n) => {
+  if (n > 100) return 'the number is too large';
+
+  const dollars = Math.floor(n);
+  let change = n - dollars;
+
+  const quarters = Math.floor(change / 0.25);
+  change -= quarters * 0.25;
+
+  const dimes = Math.floor(change / 0.1);
+  change -= dimes * 0.1;
+
+  const nickels = Math.floor(change / 0.05);
+  change -= nickels * 0.05;
+
+  const pennies = Math.round(change / 0.01);
+
+  const res = [];
+
+  if (dollars > 0) res.push(`${dollars} dollar${dollars > 1 ? 's' : ''}`);
+  if (quarters > 0) res.push(`${quarters} quarter${quarters > 1 ? 's' : ''}`);
+  if (dimes > 0) res.push(`${dimes} dime${dimes > 1 ? 's' : ''}`);
+  if (nickels > 0) res.push(`${nickels} nickel${nickels > 1 ? 's' : ''}`);
+  if (pennies > 0) res.push(`${pennies} penn${pennies > 1 ? 'ies' : 'y'}`);
+
+  return res.join(', ');
+};
 
 // Sample test cases
 console.log(calculateChange(4.62));
@@ -10,4 +37,4 @@ console.log(calculateChange(0.16));
 console.log(calculateChange(150.11));
 // $150.11 ==> Error: the number is too large
 
-// Add additional test cases here 
+// Add additional test cases here
--- hw2/02-node-routing.js
+++ hw2/02-node-routing.js
@@ -21,9 +21,59 @@ const server = http.createServer((req, res) => {
     'other',
   ];
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Node Routing Exercise');
-  res.end();
+  // main route --> /
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Node Routing Exercise');
+    res.end();
+  }
+
+  // welcome route --> /welcome
+  else if (req.url === '/welcome') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Welcome<h1>');
+    res.end();
+  }
+
+  // redirect route --> /redirect
+  else if (req.url === '/redirect') {
+    res.writeHead(302, { Location: '/redirected' });
+    res.end();
+  }
+
+  // for the redirected route when redirect is done
+  else if (req.url === '/redirected') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>You have been redrected to the /redirected<h1>');
+    res.end();
+  }
+
+  // cache route --> /cache
+  else if (req.url === '/cache') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'max-age=86400',
+    });
+    res.write('This resource was cached');
+    res.end();
+  }
+
+  // cookie route --> /cookie
+  else if (req.url === '/cookie') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Set-Cookie': 'hello=world',
+    });
+    res.write('cookies... yummm');
+    res.end();
+  }
+
+  // handle 404
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<h1>404: Page not found<h1>');
+    res.end();
+  }
 });
 
 server.listen(port, () => {
